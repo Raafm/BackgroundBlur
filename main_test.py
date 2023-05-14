eeb482f364d4f2c454187275dab0c7c933b0241a
Raafm <raafm@cin.ufpe.br>
2023-05-14 15:25:07 -0300

    refactor

--- main_test.py
+++ main_test.py
@@ -1,84 +1,60 @@
-"""# Usar este conjunto de celulas para apresentacao
-
-## definir funcoes
-"""
-
-import cv2
-import numpy as np
-import torch
-from torchvision import transforms
-
-
-from urllib.request import urlretrieve
 from PIL import Image
 from matplotlib import pyplot as plt
-from normalizar import  normalizar
-from remove_background import remove_background, load_model
-from gaussian_blurr  import gaussian_blurr_background
-from fft_blurr import blurr_background
+from gaussian_blur  import gaussian_blur_background
+from fft_blur import blur_background
 
 if __name__ == '__main__':  
 
-    """## definir tamanho das imagens"""
-
+    # definir tamanho das imagens
     plt.rcParams["figure.figsize"] = (8,5)
 
-    """## celulas para demosntracao
-
-    ### celula exemplo
-    """
-
-    input_files = []
-    print("pressione enter para terminar")
-    while True:
-        input_file = input("nome de um arquivo (com extensao jpg ou jpeg):")
-        if input_file == "":
+    print("################################ IMAGE BLUR ################################")
+    while True: 
+        print("Press Q to quit, or any other key to continue...")
+        k = input("press enter after typing to confirm...")
+        if k == 'q' or k == 'Q':
             break
         
-        input_files.append(input_file)
-
-
-    for input_file in input_files:
-        
-        
-        try:
+        input_files = []
+        print("press enter after the LAST image you wanto to blur")
+        while True:
+            input_file = input("name of a file (with extension jpg or jpeg):")
+            if input_file == "":
+                break
             
-            input_image = Image.open(input_file)
-            print("could open input image")
-            imagem_fundo_borrado = gaussian_blurr_background(input_file= input_file)
+            input_files.append(input_file)
+
+
+        for input_file in input_files:
             
-            plt.figure(figsize = (8,5))
-            plt.subplot(1,2,1)
-            plt.imshow(input_image)
-            plt.subplot(1,2,2)
-            plt.imshow(imagem_fundo_borrado)
-            plt.show()
-        except:
-            print("problemas com esta imagem, verifique a extensao e a escrita do arquivo")
+            
+            try:
+                
+                input_image = Image.open(input_file)
+                print("read image ok")
 
+                print("blurring image with gaussian kernel filter",end = " ")
+                imagem_fundo_borrado_gauss = gaussian_blur_background(input_image= input_image)
+                print(" | done")
+                print("blurring image with low frequency filter",end= " ")
+                imagem_fundo_borrado_lff = blur_background(input_image= input_image)
+                print(" | done\n")
 
+                print("ploting images")
+                plt.figure(figsize = (8,5))
+                
+                plt.title("original image")
+                plt.subplot(1,3,1)
+                plt.imshow(input_image)
 
-    input_files = []
-    print("pressione enter para terminar")
-    while True:
-        input_file = input("nome de um arquivo (com extensao jpg ou jpeg):")
-        if input_file == "":
-            break
-        
-        input_files.append(input_file)
+                plt.title("low-freq-filter")
+                plt.subplot(1,3,2)
+                plt.imshow(imagem_fundo_borrado_lff)
 
-
-    for input_file in input_files:
-        plt.figure(figsize = (8,5))
-        
-        try:
-            input_image = Image.open(input_file)
-            imagem_fundo_borrado = blurr_background(input_file= input_file)
-
-            plt.subplot(1,2,1)
-            plt.imshow(input_image)
-            plt.subplot(1,2,2)
-            plt.imshow(imagem_fundo_borrado)
-            plt.show()
-        except:
-            print("problemas com esta imagem, verifique a extensao e a escrita do arquivo")
+                plt.title("gaussian-filter")
+                plt.subplot(1,3,3)
+                plt.imshow(imagem_fundo_borrado_gauss)  
+                plt.show()
+            except:
+                print(f"problems with {input_file}, verify the extension and if the name is correct\n")
+                
